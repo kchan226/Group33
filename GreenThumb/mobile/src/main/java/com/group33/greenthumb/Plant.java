652843f1c7c30ebf03e2d89af515ea4a8da9ad9d
delicioustoast <ediwhang@gmail.com>
2015-12-01 17:51:09 -0800

    Auto generates task for demo flower

--- GreenThumb/mobile/src/main/java/com/group33/greenthumb/Plant.java
+++ GreenThumb/mobile/src/main/java/com/group33/greenthumb/Plant.java
@@ -5,6 +5,8 @@ import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
 import com.activeandroid.query.Select;
 
+import java.util.Calendar;
+
 @Table(name = "Plants")
 public class Plant extends Model {
     // NOTE: If querying the database causes your app to crash because of a SQLiteException, try uninstalling and reinstalling/rerunning the app
@@ -16,6 +18,9 @@ public class Plant extends Model {
     @Column(name = "WaterFrequency")
     public Integer waterFrequency;
 
+    @Column(name = "LastWatered")
+    public Calendar lastWatered;
+
     public Plant(){
         super();
     }
@@ -24,6 +29,7 @@ public class Plant extends Model {
         super();
         this.name = name;
         this.waterFrequency = waterFrequency;
+        this.lastWatered = Calendar.getInstance();
     }
 
     public static Plant getPlant(String name) {
