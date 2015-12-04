77ef9bbca8cbc6fb251de0788aa7af2eb1228773
brappo93 <brapponotti@berkeley.edu>
2015-12-03 20:24:16 -0800

    Added mobile/watch framer code

---
+++ Mobile.framer/imported/GreenthumbHomePage/layers.json.js
@@ -0,0 +1,187 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["GreenthumbHomePage/layers.json.js"] = [
+	{
+		"id": 3,
+		"name": "home_page",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/home_page.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 750,
+				"height": 1334
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "2048935723"
+	},
+	{
+		"id": 34,
+		"name": "add_plant_page",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/add_plant_page.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 750,
+				"height": 1334
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "828340199"
+	},
+	{
+		"id": 83,
+		"name": "to_do_list_page",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/to_do_list_page.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 750,
+				"height": 1334
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1114216488"
+	},
+	{
+		"id": 133,
+		"name": "RosesCheck",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": null,
+		"imageType": null,
+		"children": [
+			
+		],
+		"modification": "1498904447"
+	},
+	{
+		"id": 49,
+		"name": "HomeScreen_With_Notif",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": null,
+		"imageType": null,
+		"children": [
+			
+		],
+		"modification": "1721127536"
+	},
+	{
+		"id": 115,
+		"name": "Notif",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Notif.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 750,
+				"height": 1334
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1535835891"
+	},
+	{
+		"id": 145,
+		"name": "AddTask",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/AddTask.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 744,
+				"height": 1334
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1276489510"
+	},
+	{
+		"id": 173,
+		"name": "HomePageWithTask",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 750,
+			"height": 1334
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/HomePageWithTask.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 750,
+				"height": 1334
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "184960915"
+	}
+]
