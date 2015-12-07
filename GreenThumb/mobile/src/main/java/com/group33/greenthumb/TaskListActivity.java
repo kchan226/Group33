f8c5dc225fcfe3d651b7400a18e057676aa24b14
Daniel Liu <danielsliu8@gmail.com>
2015-12-07 03:23:57 -0800

    Updated Tasks to look more like Plants

--- GreenThumb/mobile/src/main/java/com/group33/greenthumb/TaskListActivity.java
+++ GreenThumb/mobile/src/main/java/com/group33/greenthumb/TaskListActivity.java
@@ -3,6 +3,14 @@ package com.group33.greenthumb;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,22 +25,28 @@ import java.util.Calendar;
 /**
  * Created by Daniel on 12/1/2015.
  */
-public class TaskListActivity extends Activity {
+public class TaskListActivity extends AppCompatActivity implements
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener, View.OnTouchListener{
 
-    ArrayList<String> taskList;
+    String DEBUG_TAG = "Task List";
+    private GestureDetectorCompat mDetector;
+    private Menu mMenu;
+    private String curQuery = "";
+    ArrayList<String> taskList = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
 
-        Button deleteButton = (Button) findViewById(R.id.minus_button);
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+        mDetector = new GestureDetectorCompat(this,this);
+        mDetector.setOnDoubleTapListener(this);
+        adapter = new ArrayAdapter<String>(this, R.layout.plant_listview, taskList);
+        String[] tasks = new String[] {"Water Roses", "Water Daisies", "Harvest Strawberriessa"};
+        taskList.addAll(Arrays.asList(tasks));
+        //listAdapter.add("task");
 
         Button addButton = (Button) findViewById(R.id.plus_button);
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -45,20 +59,19 @@ public class TaskListActivity extends Activity {
         });
 
         ListView taskListView = (ListView) findViewById(R.id.taskListView);
+        taskListView.setAdapter(adapter);
+        taskListView.setTextFilterEnabled(true);
+        registerForContextMenu(taskListView);
 
-        String[] tasks = new String[] {"[Example Task], [Example Task], [Example Task]"};
-        taskList = new ArrayList<String>();
-        taskList.addAll(Arrays.asList(tasks));
-        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, taskList);
-
-        //listAdapter.add("task");
-        taskListView.setAdapter(listAdapter);
-
-        taskListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-        });
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.task_toolbar);
+        setSupportActionBar(myToolbar);
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Your Tasks");
+        }
 
         //TODO: remove post-PROG03
         Plant rose = Plant.getPlant("rose");
@@ -74,5 +87,72 @@ public class TaskListActivity extends Activity {
 //            }
         }
 
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDown: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+                            float distanceY) {
+        Log.d(DEBUG_TAG, "onScroll: " + e1.toString()+e2.toString());
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        mDetector.onTouchEvent(event);
+        return false;
     }
 }
